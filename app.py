9fb4e15c9f2ab1f084add2b860c92f731ee5a0f3
Nagasundaramoorthy2023 <132643280+Nagasundaramoorthy2023@users.noreply.github.com>
2024-09-16 11:52:16 +0530

    Revert "Updated the code"
    This reverts commit b48c9a28f6958a6087ac0ad42b34431a724a34a0.

--- app.py
+++ app.py
@@ -17,12 +17,8 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 
 load_dotenv()
 
-os.environ['HF_API_KEY'] = os.getenv("HF_API_KEY")
+os.environ['hf_api_key'] = os.getenv("hf_api_key")
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
-
-os.environ['LANGCHAIN_API_KEY'] = os.getenv("LANGCHAIN_API_KEY")
-os.environ['LANGCHAIN_TRACING_V2'] = "true"
-os.environ['LANGCHAIN_PROJECT'] = "RAG Document Q&A"
 
 ## Set up streamlit
 
